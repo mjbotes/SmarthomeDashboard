164883963c9f1bc27abf1fc188c0947b0a4e41e9
TafLaw <tmuzeren@student.wethinkcode.co.za>
2021-05-21 01:27:20 +0200

    deal with cors

--- index.js
+++ index.js
@@ -45,11 +45,15 @@ const addDeviceBtn = document.getElementById("add-device-btn");
 const roomForm = document.getElementById("add-room-form");
 const deviceForm = document.getElementById("add-device-form");
 var selectedRoom = '';
-var url = "";
+// pull from session data
+var userId = 1;
+var url = "https://smarthome-dashboard-api.azurewebsites.net";
 
 
 const getRooms = async() => {
-    await axios.get(url)
+    await axios.get(url+"/rooms/userId/?userId="+userId, {headers: {"Access-Control-Allow-Origin": "*",
+    "Accept":"*/*"
+}})
     .then(response => {
         if (response.status == 200){
             console.log(response)
@@ -59,6 +63,18 @@ const getRooms = async() => {
     .catch(error => console.error(error));
    };
 
+function getDevices(roomId) {
+    axios.get(url+"/devices/roomId/?roomId="+roomId, {headers: {"Access-Control-Allow-Origin": "*"}})
+    .then(response => {
+        if (response.status == 200){
+            console.log(response)
+            addRoomsOptions(response.data);
+        }
+   })
+    .catch(error => console.error(error)).
+    finally(()=>{return response.data});
+}
+
 
 function setRoom() {
     selectedRoom = document.getElementById("room-select").value;
@@ -67,29 +83,69 @@ function setRoom() {
 function addRoomsOptions(responseData) {
     if (responseData == null)
         return;
+    var roomElem = document.getElementById('allrooms');
+    let index = 0;
+
+    return;
     responseData.forEach(item => {
-        var roomElem = document.getElementById('allrooms');
-        var section = document.createElement('section');
-        var sectionClassAtt = document.createAttribute('class');
-        var sectionIdAtt = document.createAttribute('id');
-        sectionClassAtt.value = "room-component";
-        sectionIdAtt.value = "room-component";
-        section.setAttribute(sectionClassAtt);
-        section.setAttribute(sectionIdAtt);
-        roomElem.appendChild(section);
+        var html = "<section class=\"room-component\"></section>";
+        roomElem.appendChild(html);
+        var devices = getDevices(item.RoomID);
+        
+        devices.forEach(device => {
+            var parent = document.getElementsByClassName("room-component")[index];
+            var deviceHtml = "\
+            <div id=\"roomName\">\
+             <h3 >"+item.RoomName+"</h3>\
+             <div class=\"tooltip\">\
+                <input type=\"button\" onclick=\"makeHidden(this.parentElement,'roomInput', 'roomName')\">\
+                <span class=\"tooltiptext\">Hide</span>\
+              </div>\
+             <div class=\"tooltip\">\
+                <input type=\"button\" onclick=\"deleteElement(this.parentElement.parentElement)\">\
+                <span class=\"tooltiptext\">Delete</span>\
+              </div>\
+            </div>\
+             <div class=\"headerbuttons\" id=\"roomInput\" hidden=\"true\"><input type=\"text\" value="+ item.RoomName +"><input type=\"button\" onclick=\"editRoomName(this, 'roomInput', 'roomName' )\"></div>\
+              <color-selector\
+              id=\"firstComp\"\
+                   draggable=\"true\"\
+                   heading="+ device.Name +"\
+                   subheading=\"Some functionality\"></color-selector>\
+                   <onoff-switch\
+              id=\"firstComp\"\
+                   draggable=\"true\"\
+                   heading="+ device.Name +"\
+                   subheading=\"Some functionality\"></onoff-switch>\
+                <add-component\
+                  heading=\"Add device\">\
+                </add-component>\
+          ";
+          parent.innerHTML = deviceHtml;
+        });
 
-        var sectionElem = document.getElementById('room-component');
-        var div = document.createElement('div');
-        var divIdAtt = document.createAttribute('id');
-        divIdAtt.value = "rootName";
-        div.setAttribute(divIdAtt);
-        sectionElem.appendChild(div);
+        index++;
+        // var section = document.createElement('section');
+        // var sectionClassAtt = document.createAttribute('class');
+        // var sectionIdAtt = document.createAttribute('id');
+        // sectionClassAtt.value = "room-component";
+        // sectionIdAtt.value = "room-component";
+        // section.setAttribute(sectionClassAtt);
+        // section.setAttribute(sectionIdAtt);
+        // roomElem.appendChild(section);
 
-        var divElem = document.getElementById('rootName');
-        var h3 = document.createElement('h3');
-        var h3Text = document.createTextNode(item.roomName);
-        h3.appendChild(h3Text);
-        divElem.appendChild(h3);
+        // var sectionElem = document.getElementById('room-component');
+        // var div = document.createElement('div');
+        // var divIdAtt = document.createAttribute('id');
+        // divIdAtt.value = "rootName";
+        // div.setAttribute(divIdAtt);
+        // sectionElem.appendChild(div);
+
+        // var divElem = document.getElementById('rootName');
+        // var h3 = document.createElement('h3');
+        // var h3Text = document.createTextNode(item.roomName);
+        // h3.appendChild(h3Text);
+        // divElem.appendChild(h3);
     });
 }
 
@@ -118,74 +174,74 @@ function closeForm(formId) {
     form.style.display = "none";
 }
 
-closeRoomsBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    closeForm("add-room-form");
-});
+// closeRoomsBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     closeForm("add-room-form");
+// });
 
-closeDevicesBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    closeForm("add-device-form");
-});
+// closeDevicesBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     closeForm("add-device-form");
+// });
 
-addRoomBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    openRoomsForm();
-});
+// addRoomBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     openRoomsForm();
+// });
 
-addDeviceBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    openDevicesForm();
-});
+// addDeviceBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     openDevicesForm();
+// });
 
-roomForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    registerRoom();
-});
+// roomForm.addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     registerRoom();
+// });
 
-deviceForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    registerDevice();
-});
+// deviceForm.addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     registerDevice();
+// });
 
-function registerRoom() {
-    let roomName = document.getElementById("room-name").value;
+// function registerRoom() {
+//     let roomName = document.getElementById("room-name").value;
 
-    //   retrieve the Id from session data
-    //   let userId = document.getElementById("room-name");
-    let userId = 0; //this is a dummy id
+//     //   retrieve the Id from session data
+//     //   let userId = document.getElementById("room-name");
+//     let userId = 0; //this is a dummy id
 
-    axios.post(url, {
-        roomName: roomName
-      })
-      .then(function (response) {
-        console.log(response);
-      });
+//     axios.post(url, {
+//         roomName: roomName
+//       })
+//       .then(function (response) {
+//         console.log(response);
+//       });
 
-    closeForm("add-room-form")
+//     closeForm("add-room-form")
 
 
 
-    console.log(roomName);
-}
+//     console.log(roomName);
+// }
 
-function registerDevice() {
-    let deviceName = document.getElementById("device-name").value;
+// function registerDevice() {
+//     let deviceName = document.getElementById("device-name").value;
     
-    //   retrieve the Id from session data
-    //   let userId = document.getElementById("room-name");
-    let userId = 0; //this is a dummy id
+//     //   retrieve the Id from session data
+//     //   let userId = document.getElementById("room-name");
+//     let userId = 0; //this is a dummy id
 
-    axios.post(url, {
-        deviceName: deviceName
-      })
-      .then(function (response) {
-        console.log(response);
-      });
-    closeForm("add-device-form")
-    console.log(deviceName);
-    console.log(selectedRoom);
-}
+//     axios.post(url, {
+//         deviceName: deviceName
+//       })
+//       .then(function (response) {
+//         console.log(response);
+//       });
+//     closeForm("add-device-form")
+//     console.log(deviceName);
+//     console.log(selectedRoom);
+// }
 
 getRooms();
 
